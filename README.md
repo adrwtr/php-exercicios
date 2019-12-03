4b5a918bd634521dca81698bedc0cd91809beb49
Adriano Waltrick <adriano@unimestre.com>
2019-12-03 07:48:58 -0300

    exercicio 2 - operadores

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Exercícios de programação feitos em PHP
 # 1 - Variáveis e impressão na tela
 php exercicio_1.php
 
-
+# 2 - Operadores
+php exercicio_2.php
---
+++ exercicio_2.php
@@ -0,0 +1,64 @@
+<?php
+$ds_enter = "\n";
+
+// aritméticos
+
+echo 1 + 1;
+echo $ds_enter;
+
+echo 1 + 1 + 1;
+echo $ds_enter;
+
+echo 3 - 1;
+echo $ds_enter;
+
+echo 5 * 2;
+echo $ds_enter;
+
+echo 5 / 2;
+echo $ds_enter;
+
+echo 5 / (2 * 2);
+echo $ds_enter;
+
+// mod
+echo 6 % 2;
+echo $ds_enter;
+
+echo 6 % 4;
+echo $ds_enter;
+
+// Relacionais
+
+echo 1 == 2;
+echo $ds_enter;
+
+echo 1 == 1;
+echo $ds_enter;
+
+// diferença
+echo 1 != 3;
+echo $ds_enter;
+
+echo 3 > 2;
+echo $ds_enter;
+
+echo 3 < 2;
+echo $ds_enter;
+
+echo 3 <= 3;
+echo $ds_enter;
+
+
+// lógicos
+
+echo 1 == 1 && 2 == 2;
+echo $ds_enter;
+
+echo 1 == 1 && 3 == 2;
+echo $ds_enter;
+
+echo 1 == 1 || 3 == 2;
+echo $ds_enter;
+
+echo "fim";
