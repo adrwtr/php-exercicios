28f5ad88333fdf1de9e4ccd8daaec1fe058f76a9
Adriano Waltrick <akuma.majunia@gmail.com>
2019-12-27 14:04:06 -0300

    exemplo de pilha

--- exercicio_9.php
+++ exercicio_9.php
@@ -31,7 +31,7 @@ class VetorArray {
     }
 
     public function getNext() {
-        $valor = isset($this->arrMemoria[$this->nr_contador]) 
+        $valor = isset($this->arrMemoria[$this->nr_contador])
             ? $this->arrMemoria[$this->nr_contador]
             : null ;
 
@@ -41,7 +41,7 @@ class VetorArray {
     }
 
     public function getElemento($i) {
-        $valor = isset($this->arrMemoria[$i-1]) 
+        $valor = isset($this->arrMemoria[$i-1])
             ? $this->arrMemoria[$i-1]
             : null ;
 
@@ -101,11 +101,12 @@ echo $ds_enter;
 
 
 
-// vetor com classes
+// LISTA simples
 // permite incluir coisas no vetor
 // permite saber o tamanho
 // permite recuperar o proximo elemento ou elemento desejado
 // permite apagar um elemento
+
 class Elemento {
     private $valor;
     private $objProxElemento;
@@ -127,7 +128,7 @@ class Elemento {
     }
 }
 
-class Vetor {
+class Lista {
     private $objElementoAtual;
     private $objElementoAnterior;
     private $objElementoInicial;
@@ -245,43 +246,319 @@ class Vetor {
     }
 }
 
-$objVetor = new Vetor();
+$objLista = new Lista();
 
-$objVetor->add(10)
+$objLista->add(10)
     ->add(20)
     ->add(30);
 
-echo $objVetor->getTamanho();
+echo $objLista->getTamanho();
 echo $ds_enter;
 
-echo $objVetor->getNext();
+echo $objLista->getNext();
 echo $ds_enter;
 
-echo $objVetor->getNext();
+echo $objLista->getNext();
 echo $ds_enter;
 
-echo $objVetor->getElemento(2);
+echo $objLista->getElemento(2);
 echo $ds_enter;
 
-echo $objVetor->getNext();
+echo $objLista->getNext();
 echo $ds_enter;
 
-echo $objVetor->getNext();
+echo $objLista->getNext();
 echo $ds_enter;
 
-echo $objVetor->removeElemento(2)
+echo $objLista->removeElemento(2)
     ->getTamanho();
 echo $ds_enter;
 
-echo $objVetor->getElemento(2);
+echo $objLista->getElemento(2);
 echo $ds_enter;
 
 
 
 // DEQUE
-// O deque é uma estrutura linear, similar a um vetor, 
-// mas com informação mantida internamente sobre a posição das 
+// O deque é uma estrutura linear, similar a um vetor,
+// mas com informação mantida internamente sobre a posição das
 // suas duas extremidades, inicial e final
+// permite incluir coisas
+// permite saber o tamanho
+// permite recuperar o proximo elemento do inicio e do fim
+// permite recuperar o elemento desejado
+// permite apagar um elemento
+
+
+class Deque {
+    private $objElementoAtual;
+    private $objElementoAnterior;
+    private $objElementoInicial;
+    private $nr_contador = 0;
+    private $nr_tamanho = 0;
+
+    public function __construct() {
+        // vamos iniciar nosso vetor
+        $this->objElementoInicial = null;
+        $this->objElementoAtual = null;
+        $this->objElementoAnterior = null;
+        $this->nr_contador = 0;
+        $this->nr_tamanho = 0;
+    }
+
+    public function push_front($valor) {
+        // cria o valor atual
+        $this->objElementoAtual = new Elemento($valor);
+
+        // joga dentro do anterior, o elemento atual
+        if ($this->objElementoAnterior != null) {
+            $this->objElementoAnterior->setProxElemento(
+                $this->objElementoAtual
+            );
+        }
+
+        // logica para ter sempre um anterior
+        $this->objElementoAnterior = $this->objElementoAtual;
+
+        // seta o elemento inicial
+        if ($this->objElementoInicial == null) {
+            $this->objElementoInicial = $this->objElementoAtual;
+        }
+
+        $this->nr_tamanho++;
+
+        return $this;
+    }
+
+    public function push_back($valor) {
+        // cria o valor atual
+        $objElemento = new Elemento($valor);
+
+        // fala que o proximo dele é o inicial, para ficar a frente
+        $objElemento->setProxElemento($this->objElementoInicial);
+
+        // seta ele como sendo o inicial
+        $this->objElementoInicial = $objElemento;
+
+        $this->nr_tamanho++;
+
+        return $this;
+    }
+
+    public function getTamanho() {
+        return $this->nr_tamanho;
+    }
+
+    public function getNext() {
+        $objElemento = $this->objElementoInicial;
+
+        for ($i = 0; $i < $this->nr_contador; $i++) {
+            if ($i == $this->nr_contador) {
+                $this->nr_contador++;
+                return $objElemento->getValor();
+            }
+
+            $objElemento = $objElemento->getProxElemento();
+
+            if ($objElemento == null) {
+                return null;
+            }
+        }
+
+        $this->nr_contador++;
+        return $objElemento->getValor();
+    }
+
+    public function getElemento($nr_posicao) {
+        $objElemento = $this->objElementoInicial;
+
+        for ($i = 0; $i <= $nr_posicao; $i++) {
+            if ($i + 1 == $nr_posicao) {
+                $this->nr_contador = $nr_posicao;
+                return $objElemento->getValor();
+            }
+
+            $objElemento = $objElemento->getProxElemento();
+
+            if ($objElemento == null) {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    public function removeElemento($nr_posicao) {
+        if ($nr_posicao == 0) {
+            $this->objElementoInicial = $this->objElementoInicial->getProxElemento();
+            $this->nr_tamanho--;
+            return $this;
+        }
+
+        $objElemento = $this->objElementoInicial;
+        $objAnterior = null;
+
+        for ($i = 0; $i <= $nr_posicao; $i++) {
+
+            if ($i + 1 == $nr_posicao) {
+                $this->nr_contador = $nr_posicao;
+
+                $objAnterior->setProxElemento(
+                    $objElemento->getProxElemento()
+                )
+                ;
+                $this->nr_tamanho--;
+                return $this;
+            }
+
+            $objAnterior = $objElemento;
+            $objElemento = $objElemento->getProxElemento();
+
+            if ($objElemento == null) {
+                return $this;
+            }
+        }
+
+        return $this;
+    }
+}
+
+$objDeque = new Deque();
+
+$nr_pos = 0;
+
+// preechendo o DEQUE na frente e atraz
+while ($nr_pos < 10) {
+    $nr_pos = $nr_pos + 1;
+    $objDeque->push_front($nr_pos);
+    $nr_pos = $nr_pos + 1;
+    $objDeque->push_back($nr_pos);
+}
+
+$nr_valor = $objDeque->getNext();
+
+// imprimindo o DEQUE
+while ($nr_valor != null) {
+    echo $nr_valor . ' ';
+    // echo $ds_enter;
+
+    $nr_valor = $objDeque->getNext();
+}
 
 
 
+
+/*
+Stack
+stack ou pilha,  uma estrutura linear com restrição na política de
+acesso aos seus elementos — a ordem de saída é inversa á ordem de entrada.
+Esta política é usualmente denominada LIFO (last in, first out), ou seja, último que entra é o primeiro que
+sai.
+
+push para inserir um elemento no topo da pilha
+pop para remover o elemento no topo da pilha.
+top para inspecionar o elemento que está no topo,
+empty para testar se a pilha está vazia
+size para obter a quantidade de elementos na pilha.
+*/
+
+class ElementoStack {
+    private $valor;
+    private $objElementoAnterior;
+
+    public function __construct($valor) {
+        $this->valor = $valor;
+    }
+
+    public function getValor() {
+        return $this->valor;
+    }
+
+    public function setElementoAnterior($objElemento) {
+        $this->objElementoAnterior = $objElemento;
+    }
+
+    public function getElementoAnterior() {
+        return $this->objElementoAnterior;
+    }
+}
+
+class Stack {
+    private $objElementoAtual;
+    private $objElementoAnterior;
+
+    private $nr_contador = 0;
+    private $nr_tamanho = 0;
+
+    public function __construct() {
+        // vamos iniciar nosso vetor
+        $this->objElementoAtual = null;
+        $this->objElementoAnterior = null;
+        $this->nr_contador = 0;
+        $this->nr_tamanho = 0;
+    }
+
+    public function push($valor) {
+
+        if ($this->objElementoAtual != null) {
+            $this->objElementoAnterior = $this->objElementoAtual;
+        }
+
+        // cria o valor atual
+        $this->objElementoAtual = new ElementoStack($valor);
+        $this->nr_tamanho++;
+
+        $this->objElementoAtual->setElementoAnterior($this->objElementoAnterior);
+
+        return $this;
+    }
+
+
+    public function pop() {
+        $valor = $this->objElementoAtual->getValor();
+
+        if ($valor = null) {
+            return null;
+        }
+
+        // remove da memoria
+        unset($this->objElementoAtual);
+
+        $this->objElementoAtual = $this->objElementoAnterior;
+        $this->objElementoAnterior = $this->objElementoAtual->getElementoAnterior();
+
+        $this->nr_tamanho--;
+
+        return $valor;
+    }
+
+    public function getTamanho() {
+        return $this->nr_tamanho;
+    }
+
+    public function top() {
+        return $this->objElementoAtual->getValor();
+    }
+}
+
+$objStack = new Stack();
+
+$objStack->push(10)
+    ->push(20)
+    ->push(30)
+    ->push(40);
+
+echo $objStack->getTamanho();
+echo $ds_enter;
+
+echo $objStack->pop();
+echo $ds_enter;
+
+echo $objStack->pop();
+echo $ds_enter;
+
+echo $objStack->top();
+echo $ds_enter;
+
+echo $objStack->getTamanho();
+echo $ds_enter;
